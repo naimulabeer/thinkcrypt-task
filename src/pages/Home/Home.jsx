bed9ad63b29e4f37601a60fee7d43000a72b24e9
naimulabeer <naimul.h.abeer@gmail.com>
2024-03-25 03:31:45 +0600

    services section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Global/Header";
+import Services from "../../components/Global/Home/Services";
 
 function Home() {
   return (
     <div>
       <Header />
+      <Services />
     </div>
   );
 }
